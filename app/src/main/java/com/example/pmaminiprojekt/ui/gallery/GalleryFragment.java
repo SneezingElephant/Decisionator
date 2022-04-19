18985268ee1cea9e18ac8a4c8e88164097c3beec
SneezingElephant <alexandrum9400@gmail.com>
2022-04-19 13:25:57 +0200

    I cannot believe my eyes

--- app/src/main/java/com/example/pmaminiprojekt/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/pmaminiprojekt/ui/gallery/GalleryFragment.java
@@ -4,18 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pmaminiprojekt.R;
 import com.example.pmaminiprojekt.databinding.FragmentGalleryBinding;
 import java.util.Random;
 
 public class GalleryFragment extends Fragment {
 
+    private static final Random RANDOM = new Random();
+    private ImageView coin;
+
+
     private FragmentGalleryBinding binding;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,10 +36,58 @@ public class GalleryFragment extends Fragment {
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
         final TextView textView = binding.presetText;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+
+
+
         return root;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        coin = (ImageView) getView().findViewById(R.id.coiniestCoin);
+        binding.flipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            flipCoin();
+            }
+        });
+    }
+
+
+
+    private void flipCoin(){
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setDuration(1000);
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                coin.setImageResource(RANDOM.nextFloat() > 0.5f ? R.drawable.coin_heads : R.drawable.coin_tails);
+
+                Animation fadeIn = new AlphaAnimation(0, 1);
+                fadeIn.setInterpolator(new DecelerateInterpolator());
+                fadeIn.setDuration(3000);
+                fadeIn.setFillAfter(true);
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        coin.startAnimation(fadeOut);
     }
 
     @Override
@@ -35,4 +95,5 @@ public class GalleryFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
