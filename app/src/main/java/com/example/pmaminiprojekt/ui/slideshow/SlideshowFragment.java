8e4616c8ebb779285b2bfef60eb5c5591f9ffd13
Markus <xp73vc@student.aau.dk>
2022-04-20 10:52:04 +0200

    WHEEL

--- app/src/main/java/com/example/pmaminiprojekt/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/pmaminiprojekt/ui/slideshow/SlideshowFragment.java
@@ -4,17 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pmaminiprojekt.R;
 import com.example.pmaminiprojekt.databinding.FragmentSlideshowBinding;
+
+import java.util.Random;
 
 public class SlideshowFragment extends Fragment {
 
     private FragmentSlideshowBinding binding;
+
+    private static final String [] sectors ={"Burger", "Kebab", "Chinese", "Pizza"};
+    private static final int [] sectorDegrees = new int[sectors.length];
+    private static final Random random = new Random();
+    private int degree = 0;
+    private boolean isSpinning = false;
+
+    private ImageView wheel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +41,27 @@ public class SlideshowFragment extends Fragment {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        wheel = (ImageView) getView().findViewById(R.id.wheel);
+
+        getDegreeForSectors();
+
+        binding.spinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isSpinning)
+                {
+                    spin();
+                    isSpinning = true;
+                }
+            }
+        });
+
     }
 
     @Override
@@ -34,4 +69,50 @@ public class SlideshowFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    private void spin()
+    {
+        degree = random.nextInt(sectors.length-1);
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0,(360 * sectors.length) + sectorDegrees[degree],
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+        rotateAnimation.setDuration(3600);
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setInterpolator(new DecelerateInterpolator());
+        rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation)
+            {
+                isSpinning = false;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        wheel.startAnimation(rotateAnimation);
+
+    }
+
+
+    private void getDegreeForSectors()
+    {
+        int sectorDegree = 360/sectors.length;
+
+        for (int i=0; i < sectors.length; i++)
+        {
+            sectorDegrees[i] = (i+1) * sectorDegree;
+        }
+    }
+
+
+
 }
