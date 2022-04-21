4c42ec0f9a103004a8d3d316a7b80eec56b8e0a0
RasmusKanne <rasmuskanne@gmail.com>
2022-04-21 10:37:56 +0200

    wheelspin presets

--- app/src/main/java/com/example/pmaminiprojekt/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/pmaminiprojekt/ui/slideshow/SlideshowFragment.java
@@ -29,12 +29,21 @@ public class SlideshowFragment extends Fragment {
     private FragmentSlideshowBinding binding;
 
     private static final String [] sectors ={"Burger", "Kebab", "Chinese", "Pizza"};
+    private static final String [] activitys ={"Exercise", "Movie", "Clean Up", "Find Friends", "Video Games"};
+    private static final String [] gifts ={"Flowers", "Chocolate", "Gift Card", "Book", "Clothes", "Game"};
     private static final int [] sectorDegrees = new int[sectors.length];
+    private static final int [] activitysDegrees = new int[activitys.length];
+    private static final int [] giftsDegrees = new int[gifts.length];
     private static final Random random = new Random();
     private int degree = 0;
     private boolean isSpinning = false;
+    private boolean isTakeaway = true;
+    private boolean isActivity = false;
+    private boolean isGift = false;
 
     private ImageView wheel;
+    private ImageView wheelActivitys;
+    private ImageView wheelGifts;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -51,8 +60,12 @@ public class SlideshowFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         wheel = (ImageView) getView().findViewById(R.id.wheel);
+        wheelActivitys = (ImageView) getView().findViewById(R.id.wheelActivity);
+        wheelGifts = (ImageView) getView().findViewById(R.id.wheelGifts);
 
         getDegreeForSectors();
+        getDegreeForActivitys();
+        getDegreeForGifts();
 
         binding.spinBtn.setOnClickListener(new View.OnClickListener() {
 
@@ -68,10 +81,50 @@ public class SlideshowFragment extends Fragment {
         binding.takeawayPreset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (!isSpinning && !isTakeaway) {
+                    isTakeaway = true;
+                    isActivity = false;
+                    isGift = false;
 
+                    wheel.setAlpha(1f);
+                    wheelActivitys.setAlpha(0f);
+                    wheelGifts.setAlpha(0f);
+
+                }
             }
         });
 
+        binding.activityPreset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isSpinning && !isActivity) {
+                    isActivity = true;
+                    isTakeaway = false;
+                    isGift = false;
+
+                    wheel.setAlpha(0f);
+                    wheelActivitys.setVisibility(View.VISIBLE);
+                    wheelActivitys.setAlpha(1f);
+                    wheelGifts.setAlpha(0f);
+                }
+            }
+        });
+
+        binding.giftIdeasPreset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isSpinning && !isGift) {
+                    isGift = true;
+                    isActivity = false;
+                    isTakeaway = false;
+
+                    wheel.setAlpha(0f);
+                    wheelActivitys.setAlpha(0f);
+                    wheelGifts.setVisibility(View.VISIBLE);
+                    wheelGifts.setAlpha(1f);
+                }
+            }
+        });
     }
 
     @Override
@@ -82,33 +135,92 @@ public class SlideshowFragment extends Fragment {
 
     private void spin()
     {
-        degree = random.nextInt(sectors.length-1);
+        if (isTakeaway) {
+            degree = random.nextInt(sectors.length - 1);
 
-        RotateAnimation rotateAnimation = new RotateAnimation(0,(360 * sectors.length) + sectorDegrees[degree],
-                RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+            RotateAnimation rotateAnimation = new RotateAnimation(0, (360 * sectors.length) + sectorDegrees[degree],
+                    RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
 
-        rotateAnimation.setDuration(3600);
-        rotateAnimation.setFillAfter(true);
-        rotateAnimation.setInterpolator(new DecelerateInterpolator());
-        rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
+            rotateAnimation.setDuration(3600);
+            rotateAnimation.setFillAfter(true);
+            rotateAnimation.setInterpolator(new DecelerateInterpolator());
+            rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
 
-            }
+                }
 
-            @Override
-            public void onAnimationEnd(Animation animation)
-            {
-                isSpinning = false;
-            }
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    isSpinning = false;
+                }
 
-            @Override
-            public void onAnimationRepeat(Animation animation) {
+                @Override
+                public void onAnimationRepeat(Animation animation) {
 
-            }
-        });
+                }
+            });
 
-        wheel.startAnimation(rotateAnimation);
+            wheel.startAnimation(rotateAnimation);
+        }
+        else if (isActivity) {
+            degree = random.nextInt(activitys.length-1);
+
+            RotateAnimation rotateAnimation = new RotateAnimation(0,(360 * activitys.length) + activitysDegrees[degree],
+                    RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+            rotateAnimation.setDuration(3600);
+            rotateAnimation.setFillAfter(true);
+            rotateAnimation.setInterpolator(new DecelerateInterpolator());
+            rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation)
+                {
+                    isSpinning = false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            wheelActivitys.startAnimation(rotateAnimation);
+        }
+        else if (isGift) {
+            degree = random.nextInt(gifts.length-1);
+
+            RotateAnimation rotateAnimation = new RotateAnimation(0,(360 * gifts.length) + giftsDegrees[degree],
+                    RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+            rotateAnimation.setDuration(3600);
+            rotateAnimation.setFillAfter(true);
+            rotateAnimation.setInterpolator(new DecelerateInterpolator());
+            rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation)
+                {
+                    isSpinning = false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            wheelGifts.startAnimation(rotateAnimation);
+        }
 
     }
 
@@ -120,6 +232,26 @@ public class SlideshowFragment extends Fragment {
         for (int i=0; i < sectors.length; i++)
         {
             sectorDegrees[i] = (i+1) * sectorDegree;
+        }
+    }
+
+    private void getDegreeForActivitys()
+    {
+        int activityDegree = 360/activitys.length;
+
+        for (int i=0; i < activitys.length; i++)
+        {
+            activitysDegrees[i] = (i+1) * activityDegree;
+        }
+    }
+
+    private void getDegreeForGifts()
+    {
+        int giftDegree = 360/gifts.length;
+
+        for (int i=0; i < gifts.length; i++)
+        {
+            giftsDegrees[i] = (i+1) * giftDegree;
         }
     }
 
